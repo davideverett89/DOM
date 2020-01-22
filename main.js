0530e7dc469f394aeb3d6bbfe5f723e26cea15ab
David Everett <davideverett1989@gmail.com>
2020-01-21 19:56:34 -0600

    Did some loops and made stuff in the DOM.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("DOM loops");
+// const animals = ["Dog", "Cat", "Fish", "Bird", "Pig"];
+
+
+// const target = document.getElementById("target");
+// let orderedList = document.createElement("ol");
+// target.appendChild(orderedList);
+
+// for (let i = 0; i < animals.length; i++) {
+//     let currentAnimal = animals[i];
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = currentAnimal;
+//     orderedList.appendChild(listItem);
+// }
